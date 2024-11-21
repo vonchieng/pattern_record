00e6b5d6d60b02ed60babde3d6851ef21532e968
vonchieng <eyvonchieng123@icloud.com>
2024-11-21 13:59:12 +0800

    Initial

--- lib/data.dart
+++ lib/data.dart
@@ -8,29 +8,29 @@ class Document {
   (String, {DateTime modified}) get metadata {
     // Add from here...
     if (_json // Modify from here...
-    case {
-    'metadata': {
-    'title': String title,
-    'modified': String localModified,
-    }
-    }) {
+        case {
+          'metadata': {
+            'title': String title,
+            'modified': String localModified,
+          }
+        }) {
       return (title, modified: DateTime.parse(localModified));
     } else {
       throw const FormatException('Unexpected JSON');
     }
   }
 
-  List<Block> getBlocks() {                                  // Add from here...
-      if (_json case {'blocks': List blocksJson}) {
-        return [for (final blockJson in blocksJson) Block.fromJson(blockJson)];
-      } else {
-        throw const FormatException('Unexpected JSON format');
-      }
+  List<Block> getBlocks() {
+    // Add from here...
+    if (_json case {'blocks': List blocksJson}) {
+      return [for (final blockJson in blocksJson) Block.fromJson(blockJson)];
+    } else {
+      throw const FormatException('Unexpected JSON format');
     }
   }
+}
 
-
-  const documentJson = '''
+const documentJson = '''
 {
   "metadata": {
     "title": "My Document",
@@ -54,36 +54,22 @@ class Document {
 }
 ''';
 
-
-class Block {
-  final String type;
-  final String text;
-
-  Block(this.type, this.text);
-
-  factory Block.fromJson(Map<String, dynamic> json) {
-    //factory = can create a instance from the stock = make new one if the type not in json
-    if (json case {'type': final type, 'text': final text}) {
-      return Block(type, text);
-    } else {
-      throw const FormatException('Unexpected JSON format');
-    }
-  }
-}
-
 class HeaderBlock extends Block {
   final String text;
+
   HeaderBlock(this.text);
 }
 
 class ParagraphBlock extends Block {
   final String text;
+
   ParagraphBlock(this.text);
 }
 
 class CheckboxBlock extends Block {
   final String text;
   final bool isChecked;
+
   CheckboxBlock(this.text, this.isChecked);
 }
 
@@ -95,7 +81,7 @@ sealed class Block {
       {'type': 'h1', 'text': String text} => HeaderBlock(text),
       {'type': 'p', 'text': String text} => ParagraphBlock(text),
       {'type': 'checkbox', 'text': String text, 'checked': bool checked} =>
-          CheckboxBlock(text, checked),
+        CheckboxBlock(text, checked),
       _ => throw const FormatException('Unexpected JSON format'),
     };
   }
